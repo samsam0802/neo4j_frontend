d3c5d0cfcfedfa0a540cfad98448b1900a4f4231
minsuk <kmsk0802@gmail.com>
2026-01-30 17:07:43 +0900

    commit

--- src/api/soraApi.jsx
+++ src/api/soraApi.jsx
@@ -1,5 +1,5 @@
-export const soraGet = async (payload) => {
-  const res = await axio.get(`https://neo4jbackend-production-7fb0.up.railway.app`, payload);
+export const soraGet = async () => {
+  const res = await axio.get(`https://neo4jbackend-production-7fb0.up.railway.app`);
 
   return res.data;
 };
