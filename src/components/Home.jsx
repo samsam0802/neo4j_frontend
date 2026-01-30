6a551e43acefbf00ff581a7d47d2533c85c67d2e
minsuk <kmsk0802@gmail.com>
2026-01-30 17:00:46 +0900

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import { soraGet } from '../api/soraApi'
+import { useEffect, useState } from "react";
+import { soraGet } from "../api/soraApi";
 
-const Home = () => {
-  const r = soraGet()
-    return (
-    <div>Home,{r}</div>
-  )
+function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    soraGet().then((res) => {
+      setData(res); // API 응답 데이터를 상태에 저장
+    }).catch(err => {
+      console.error("데이터 로드 실패:", err);
+    });
+  }, []);
+
+  if (!data) return <div>로딩 중이거나 백엔드가 꺼져있습니다...</div>;
+
+  return (
+    <div>
+      <h1>백엔드 메시지: {data.message}</h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
