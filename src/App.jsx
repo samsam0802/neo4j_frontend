4b93623d1ba46f35ce739da9c0960ba9f06824aa
minsuk <kmsk0802@gmail.com>
2026-01-30 17:06:17 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { RouterProvider } from 'react-router-dom'
+import root from './routers/root'
 
 function App() {
 
